769872de402cd5a927d02430622424009250f382
BadFatRat <61916463+BadFatRat@users.noreply.github.com>
2020-10-12 18:08:38 +0300

    Исправил 2.0

--- DZ_2/Program.cs
+++ DZ_2/Program.cs
@@ -15,48 +15,57 @@ namespace DZ_2
                 x1 = -9;
                 x2 = -3;
             }
-            if (R < 3)
-                Console.WriteLine($"Функция не определена на промежутке [-9, {x1}).");
-            if (R == 0)
-                Console.WriteLine($"y({-6:0.00}) = {0:0.000}");
+            else if (R == 0)
+                Console.WriteLine($"y({-6:0.0}) = {0:0.###}");
+            else if (R != 3)
+            {
+                x1 = CuteConsoleOutput(x1);
+                x2 = CuteConsoleOutput(x2);
+                Console.WriteLine($"Функция не определена на промежутке [-9, {-6 - R:0.###}).");
+            }
             for (double x = x1; x <= -3; x += 0.2) 
             {
-                if (x < -6)
-                    Console.WriteLine($"y({x:0.00}) = {Sector1(R, x):0.000}");
-                else if (x <= -3)
-                    Console.WriteLine($"y({x:0.00}) = {Sector2(x):0.000}");
+                if (Math.Round(x, 1) == -6.0 && R != 0 && R != 3)
+                    Console.WriteLine($"y({-6:0.0}) = {Sector1(R, x):0.###}");
+                if (x < -6.0)
+                    Console.WriteLine($"y({x:0.0}) = {Sector1(R, x):0.###}");
+                else if (x <= -3.0)
+                    Console.WriteLine($"y({x:0.0}) = {Sector2(x):0.###}");
             }
             if (R < 3)
             {
-                Console.WriteLine($"y({-3.0:0.00}) = {Sector2(-3.0):0.000}");
-                Console.WriteLine($"Функция не определена на промежутке (-3; {-R})");
+                Console.WriteLine($"y({-3.0:0.0}) = {Sector2(-3.0):0.###}");
+                Console.WriteLine($"Функция не определена на промежутке (-3; {-R:0.###})");
             }
             if (R == 0)
-                Console.WriteLine($"y({0:0.00}) = {0:0.000}");
-            for (double x = x2; x <= 9; x += 0.2) 
+                Console.WriteLine($"y({0:0.0}) = {0:0.###}");
+            for (double x = x2; x <= 9.0; x += 0.2) 
             {
-                if (x < 0)
-                {
-                    if (R < 0.01)
-                        Console.WriteLine($"y({Math.Abs(x):0.00}) = {Sector3(R, x):0.000}");
-                    else
-                        Console.WriteLine($"y({x:0.00}) = {Sector3(R, x):0.000}");
-                }
-                else if (x < 3)
-                    Console.WriteLine($"y({Math.Abs(x):0.00}) = {Sector4(x):0.000}");
-                else if (x < 9)
-                    Console.WriteLine($"y({x:0.00}) = {Sector5(x):0.000}");
+                if (CuteConsoleOutput(x) == 0.0 && R != 0.0)
+                    Console.WriteLine($"y({0:0.0}) = {Sector3(R, x):0.###}");
+                if (CuteConsoleOutput(x) < 0.0)
+                        Console.WriteLine($"y({x:0.0}) = {Sector3(R, x):0.###}");
+                else if (x < 3.0)
+                    Console.WriteLine($"y({Math.Abs(x):0.0}) = {Sector4(x):0.###}");
+                else if (x < 9.0)
+                    Console.WriteLine($"y({x:0.0}) = {Sector5(x):0.###}");
+                
             }
-            Console.WriteLine($"y({9.0:0.00}) = {Sector5(9.0):0.000}");
+            Console.WriteLine($"y({9.0:0.0}) = {Sector5(9.0):0.###}");
         }
         static double CheckR(string user_input) // проверяет, является ли введённое значение числом типа double в нужном диапазоне (0 >= R)
         {
             if (double.TryParse(user_input, out double value))
-                if (value >= 0)
+                if (value >= 0.0)
                     return value;
             Console.WriteLine("Введённая вами строка не является нужным числом. Конец программы.");
             Environment.Exit(0);
             return 0;
+        }
+        static double CuteConsoleOutput(double x)
+        {
+            x = Math.Truncate(x * 10.0) / 10.0;
+            return x;
         }
         static double Sector1(double R, double x)
         {
